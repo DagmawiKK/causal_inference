e5f46fd23781f81e7e18a9c4f420810f16b7647b
dagmawikk <dagmawikassa1@gmail.com>
2025-05-19 13:21:08 +0300

    Include psm Router in app

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
+from .api import routes
 
 app = FastAPI(title = "Causal Inference Analysis Tool")
+app.include_router(routes.router, prefix="/api", tags=["routes"])
 
 @app.get("/")
 async def root():
