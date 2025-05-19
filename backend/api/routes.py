904d5504f5200439ceb50429a94f6daf9b0f097f
dagmawikk <dagmawikassa1@gmail.com>
2025-05-19 13:18:49 +0300

    Implement route using APIRouter

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,25 +1,23 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware # To allow requests from Streamlit
+from fastapi import FastAPI, HTTPException, APIRouter
 from .schemas import PSMRequest, PSMResponse
-from ..main import app
-from core.psm import psm
-from core.dml import dml
+from ..core.psm import psm
+# from core.dml import dml
+router = APIRouter()
 
-
-@app.post("/psm", response_model = PSMResponse)
+@router.post("/psm", response_model = PSMResponse)
 async def psm_endpoint(request: PSMRequest):
   result = psm(
-    data = request.data
-    treatment_col = request.treatment_col,
-    outcome_col = request.outcome_col,
-    counfounders = request.counfounders,
-    n_neighbors = request.n_neighbors,
-    random_state = request.random_state,
-    scale_features = request.scale_features,
-    use_caliper = request.use_caliper,
-    caliper = request.caliper,
-    show_prop_hist = request.show_prop_hist
-    show_matched_pair_hist = request.show_matched_pair_hist
+      data=request.data,
+      treatment_col=request.treatment_col,
+      outcome_col=request.outcome_col,
+      confounders=request.confounders, 
+      n_neighbors=request.n_neighbors,
+      random_state=request.random_state,
+      scale_features=request.scale_features,
+      use_caliper=request.use_caliper,
+      caliper=request.caliper,
+      show_prop_hist=request.show_prop_hist,
+      show_matched_pair_hist=request.show_matched_pair_hist
   )
 
   return result
